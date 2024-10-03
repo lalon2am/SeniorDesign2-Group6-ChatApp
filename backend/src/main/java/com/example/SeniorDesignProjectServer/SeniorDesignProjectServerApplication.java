f60590a72c7a5b883be0b60e7af6913a87ce2db5
lalon2am <arjay.lalonde@gmail.com>
2024-10-03 18:19:33 -0400

    Add backend folder

---
+++ backend/src/main/java/com/example/SeniorDesignProjectServer/SeniorDesignProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SeniorDesignProjectServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorDesignProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorDesignProjectServerApplication.class, args);
+	}
+
+}
