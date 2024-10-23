13cf50fd33d6911c804cfd9ee8c75eb37e283c21
drewburton <drew.burton.gc@gmail.com>
2024-10-23 12:04:00 -0400

    added test to force controller to call service to get messages

---
+++ backend/src/main/java/com/example/springboot/WebAppService.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WebAppService {
+    public List<WebAppController.Message> getChats() {
+        return null;
+    }
+}
--- backend/src/test/java/com/example/springboot/WebAppControllerTest.java
+++ backend/src/test/java/com/example/springboot/WebAppControllerTest.java
@@ -2,14 +2,17 @@ package com.example.springboot;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +31,9 @@ public class WebAppControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	@Mock
+	private WebAppService service;
+
 //	@Test
 //	public void getHello() throws Exception {
 //		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
@@ -43,12 +49,31 @@ public class WebAppControllerTest {
 		mapper.registerModule(module);
 		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
+		WebAppController.Message message = new WebAppController.Message("test", "test", Instant.MIN);
+		String expected = mapper.writeValueAsString(List.of(message));
 
-		String expected = mapper.writeValueAsString(List.of(new WebAppController.Message("test", "test", Instant.MIN)));
+		when(service.getChats()).thenReturn(List.of(message));
 
 		// act
 		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andReturn();
+		String responseString = result.getResponse().getContentAsString();
+
+		// assert
+		assertEquals(expected, responseString);
+	}
+
+	@Test
+	public void emptyChatsReturnsNoContent() throws Exception {
+		// arrange
+		ObjectMapper mapper = new ObjectMapper();
+		String expected = mapper.writeValueAsString(List.of());
+
+		when(service.getChats()).thenReturn(List.of());
+
+		// act
+		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent()).andReturn();
 		String responseString = result.getResponse().getContentAsString();
 
 		// assert
