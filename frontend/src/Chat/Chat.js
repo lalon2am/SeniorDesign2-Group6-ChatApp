c0015d45a01c14b015030ac39c990a6be31fb849
drewburton <drew.burton.gc@gmail.com>
2024-10-24 10:10:22 -0400

    fixed error from previous lab on frontend

--- frontend/src/Chat/Chat.js
+++ frontend/src/Chat/Chat.js
@@ -8,29 +8,28 @@ function Chat({ isOpen }) {
     { id: 2, sender: 'Bob', text: 'I am good, thanks! How about you?', timestamp: new Date('2024-01-01T14:30:00.000Z').getTime() + Math.floor(Math.random() * 86400000) },
     { id: 3, sender: 'Alice', text: 'I am doing well, thank you!', timestamp: new Date('2024-01-01T14:30:00.000Z').getTime() + Math.floor(Math.random() * 86400000) },
     { id: 4, sender: 'Charlie', text: 'Hey everyone!', timestamp: new Date('2024-01-01T14:30:00.000Z').getTime() + Math.floor(Math.random() * 86400000) },
-];
+  ];
 
   return (
     <div>
-        <h2>Messages</h2>
-        <div>
+      <h2>Messages</h2>
+      <div>
         {messages.map((message) => {
-    const date = new Date(message.timestamp);
-    const formattedTime = `${date.toLocaleDateString()}}`;
-    console.log(formattedTime); // Log the formatted time
-    return (
-        <Message
-        key={formattedTime}
-        text={message.text}
-        user={message.sender}
-        timestamp={formattedTime}
-    />
-        
-    );
-})}
-        </div>
+          const date = new Date(message.timestamp);
+          const formattedTime = `${date.toLocaleDateString()}`;
+          return (
+            <Message
+              key={message.id}
+              text={message.text}
+              user={message.sender}
+              timestamp={formattedTime}
+            />
+
+          );
+        })}
+      </div>
     </div>
-);
+  );
 }
 
 export default Chat;
